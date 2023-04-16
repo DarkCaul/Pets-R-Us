8109a0bff6461b2ff3fc926b9e1739937e34ab3c
Malikai Baker <DarkCaul@outlook.com>
2023-04-16 14:58:11 -0500

    added partial images and views updated styles grooming and index

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-
+//views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use('/styles', express.static(__dirname + 'public/styles'));
+app.use('/images', express.static(__dirname + 'public/images'));
 
 
 //renders homepage
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
     res.render('index');
 });
 
@@ -26,8 +27,13 @@ app.get('/grooming', (req, res) => {
     res.render('grooming');
 });
 
+app.get('/boarding', (req, res) => {
+    res.render('boarding');
+});
 
-
+app.get('/training', (req, res) => {
+    res.render('training');
+});
 
 
 //listening on port 3000
